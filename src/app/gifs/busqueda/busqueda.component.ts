477f3693566d6268092337fc1c7be9345c33af07
Fabián Montoya <jose.fabian.montoya9@gmail.com>
2022-11-27 15:42:43 -0500

    Gifs service

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,19 +1,22 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 
+import { GifsService } from '../services/gifs.service';
+
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.component.html',
 })
 export class BusquedaComponent {
-  constructor() {}
-
   // https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-0.html#non-null-assertion-operator
   @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
+
+  constructor(private gifsService: GifsService) {}
 
   buscar() {
     // console.log(termino);
     const valor = this.txtBuscar.nativeElement.value;
-    console.log(valor);
+    // console.log(valor);
+    this.gifsService.buscarGifs(valor);
     this.txtBuscar.nativeElement.value = '';
   }
 }
