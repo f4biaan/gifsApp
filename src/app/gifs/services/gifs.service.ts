eb443684747522b9e078bdf568f1ef0f18074f82
Fabián Montoya <jose.fabian.montoya9@gmail.com>
2022-11-28 14:35:30 -0500

    Local storage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -15,13 +15,20 @@ export class GifsService {
     return [...this._historial];
   }
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+    /* if ( localStorage.getItem('historial') ) {
+      this._historial = JSON.parse(localStorage.getItem('historial')!);
+    } */
+  }
 
   buscarGifs(query: string) {
     query = query.trim().toLowerCase();
     if (!this._historial.includes(query)) {
       this._historial.unshift(query);
       this._historial = this._historial.splice(0, 10);
+
+      localStorage.setItem('historial', JSON.stringify(this._historial))
     }
     // console.log(this._historial);
     this.http
