5b38962101f940550b149ba11c0e67356a9bfa3b
Fabián Montoya <jose.fabian.montoya9@gmail.com>
2022-11-28 14:20:13 -0500

    Consumo API

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,21 +1,36 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { SearchGifsResponse, Gif } from '../interfaces/gifs.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GifsService {
+  private apiKey: string = 't4Yf9JtbH5VOXx1ztCuRqjQ3CzcETpXu';
   private _historial: string[] = [];
+
+  public resultados: Gif[] = [];
 
   get historial() {
     return [...this._historial];
   }
 
+  constructor(private http: HttpClient) {}
+
   buscarGifs(query: string) {
     query = query.trim().toLowerCase();
-    if ( !this._historial.includes(query) ) {
+    if (!this._historial.includes(query)) {
       this._historial.unshift(query);
-      this._historial = this._historial.splice(0,10);
+      this._historial = this._historial.splice(0, 10);
     }
-    console.log(this._historial);
+    // console.log(this._historial);
+    this.http
+      .get<SearchGifsResponse>(
+        `https://api.giphy.com/v1/gifs/search?api_key=t4Yf9JtbH5VOXx1ztCuRqjQ3CzcETpXu&q=${query}&limit=10`
+      )
+      .subscribe((resp) => {
+        console.log(resp.data);
+        this.resultados = resp.data;
+      });
   }
 }
